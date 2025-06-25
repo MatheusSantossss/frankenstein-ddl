04c7348e31e7d598b508b2ee5d93270e5b207da0
Matheus Henrique <henrique7508@gmail.com>
2025-06-25 08:17:12 -0300

    Criação do projeto flyway

---
+++ src/main/kotlin/br/com/frankenstein/frankensteinddl/FrankensteinDdlApplication.kt
@@ -0,0 +1,11 @@
+package br.com.frankenstein.frankensteinddl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrankensteinDdlApplication
+
+fun main(args: Array<String>) {
+    runApplication<FrankensteinDdlApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/frankenstein/frankensteinddl/FrankensteinDdlApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.frankenstein.frankensteinddl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FrankensteinDdlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
